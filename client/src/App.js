6a37b5adc0003734b758ccd55c2064dba4701762
pavannaik43 <pavannaik43@users.noreply.github.com>
2026-09-23 18:14:31 +0530

    build: add root package.json and vercel.json for automatic Vercel builds

--- client/src/App.js
+++ client/src/App.js
@@ -138,6 +138,7 @@ export default function App() {
     } finally {
       setLoading(false);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentUser?.id, currentUser?.role]);
 
   useEffect(() => {
